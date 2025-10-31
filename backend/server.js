5a502ab4a401be1c2be43c18157ae900cf03fd99
Praveen kumar S <praveen0031kumar@gmail.com>
2025-10-31 22:24:33 +0530

    healthy production

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 // Routes
 app.use('/coaches', coachRoutes);
 
